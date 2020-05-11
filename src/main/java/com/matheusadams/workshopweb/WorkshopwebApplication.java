82754b521725d9156fe881d917db383218f33087
Matheus Adams <adamsmatheus7@gmail.com>
2020-05-11 19:29:10 -0300

    Criado o BD e configurado URL

---
+++ src/main/java/com/matheusadams/workshopweb/WorkshopwebApplication.java
@@ -0,0 +1,13 @@
+package com.matheusadams.workshopweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusadams/workshopmongo/WorkshopwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusadams.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
